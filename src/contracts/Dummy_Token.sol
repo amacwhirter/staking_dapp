a982016f6c1b4fd13e590cfb5b44059c318920b8
Ashley <macwhirtera@gmail.com>
2022-01-31 17:55:33 -0500

    add erc-20 tokens, add smart contract

---
+++ src/contracts/Dummy_Token.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <=0.4.22 <0.9.0;
+
+contract Dummy {
+
+    string public name = "dummy token";
+    string public symbol = "dum token";
+    uint public totalsupply = 1000000000000000000000000;
+    uint public decimal = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint256) public balance;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() public {
+        balance[msg.sender] = totalsupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns(bool success) {
+        require(balance[msg.sender] <= _value);
+        balance[msg.sender] -= _value;
+        balance[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approve(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(_value <= balance[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balance[_from] -= _value;
+        balance[_to] += _value;
+        allowance[_from][msg.sender] -= +value;
+        emit transfer(_from, _to, _value);
+        return true;
+    }
+}
---
+++ src/contracts/Staking_Dapp.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <=0.4.22 <0.9.0;
+
+import "./Dummy_Token.sol";
+import "./Tether_Token.sol";
+
+contract Staking_Dapp {
+
+    string public name = "Staking Dapp";
+    address public owner;
+    Dummy public dummy_token;
+    Tether public tether_token;
+
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasstaked;
+    mapping(address => bool) public isstaking;
+
+    constructor(Dummy _dummyToken, Tether _tetherToken) public {
+        dummy_token = _dummyToken;
+        tether_token = _tetherToken;
+        owner = msg.sender;
+    }
+
+    function stakeTokens(uint _amount) public {
+        // check if amount is zero or not
+        require(_amount > 0, "Amount can not be zero.");
+        // transferred to tether contract address
+        tether_token.transfer(msg.sender, address(this), _amount);
+        // updated staking balance of user
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // added user to the stakers array
+        if (!hasstaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // updated the status of staking or not
+        isstaking[msg.sender] = true;
+        hasstaked[msg.sender] = true;
+    }
+
+    function unstakeTokens() public {
+        // fetched balance of staker into a variable
+        uint balance = stakingBalance[msg.sender];
+        // check if balance is zero or not
+        require(balance > 0, "Staking balance is zero.");
+
+        // we transferred back tether tokens to use
+        tether_token.transfer(msg.sender, balance);
+
+        // set the staking balance to 0
+        stakingBalance[msg.sender] = 0;
+
+        // updated the staking status
+        isstaking[msg.sender] = false;
+    }
+
+    function issuedummy() public {
+        // check if owner is accessing function or not
+        require(msg.sender == owner, "Caller must be the owner for this function");
+
+        // iterate through stakers array used to store addressed of stakers
+        for (uint i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            // store balance for each staker
+            uint balance = stakingBalance[recipient];
+            // check if balance is greater to zero or not, if balance greater than zero, transfer dummy tokens
+            if (balance > 0) {
+                dummy_token.transfer(recipient, balance);
+            }
+        }
+    }
+}
